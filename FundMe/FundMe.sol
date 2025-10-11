e3fd4616aeba6d9074d031a781ad19fa619de6b2
Vanvala <geos79@tut.by>
2025-10-11 03:22:24 +0300

    create FundMe.sol

---
+++ FundMe/FundMe.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+contract FundMe{
+
+    function fund() public payable {
+        require(msg.value >= 1e18, "did't send enough EHT!");
+    }
+
+    // function withdraw() public {}
+}
